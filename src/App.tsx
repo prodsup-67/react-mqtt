6ae53aa08ffb4ca8488c95128df1fc6265c296c8
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-11-30 17:17:55 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,41 @@ import { useEffect, useState } from "react";
 import mqtt from "mqtt";
 import { MQTT_PORT, MQTT_URL } from "./env";
 
-const client = mqtt.connect(`mqtt://${MQTT_URL}`, {
-  port: MQTT_PORT,
-});
-
 interface DataArr {
   topic: string;
   message: string;
   timestamp: number;
 }
 
+const client = mqtt.connect(`mqtt://${MQTT_URL}`, {
+  port: MQTT_PORT,
+});
+
 function App() {
   const [dataArr, setDataArr] = useState<DataArr[]>([]);
   const [isConnected, setIsConnected] = useState(false);
+  const [isSubbed, setIsSubbed] = useState(false);
+  const [err, setErr] = useState("");
 
   useEffect(() => {
-    if (!isConnected) {
+    if (client && !isConnected) {
       client.on("connect", () => {
         console.log("Connected");
         setIsConnected(true);
       });
     }
 
-    return () => {
-      // console.log("Exiting...");
-      // setIsConnected(false);
-      // client.end();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (isConnected) {
+    if (client && isConnected && !isSubbed) {
       client.subscribe("test_mqtt", (err) => {
-        if (!err) {
+        if (err) {
+          setIsSubbed(false);
+          setErr("Error subscribing");
+        } else {
           console.log("Subscribed to test_mqtt");
-          setIsConnected(true); // Prevent double subscription
+          setIsSubbed(true);
+          setErr("");
         }
       });
-
       client.on("message", (topic, message) => {
         const data = {
           topic: topic,
@@ -49,7 +46,9 @@ function App() {
         setDataArr((prev) => [data, ...prev]);
       });
     }
-  }, [isConnected]);
+  }, [client, isConnected, isSubbed]);
+  if (!isConnected) return <h1>Not Connected to MQTT</h1>;
+  if (err) return <h1>Error: {err}</h1>;
 
   return (
     <>
